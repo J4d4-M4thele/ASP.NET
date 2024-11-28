ea68a760595559e5de65eb24fe76d39ccb546cad
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-11-28 11:59:16 +0200

    first commit

---
+++ RestaurantList/Models/Restaurant.cs
@@ -0,0 +1,10 @@
+﻿namespace RestaurantList.Models
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public string Address { get; set; }
+    }
+}
