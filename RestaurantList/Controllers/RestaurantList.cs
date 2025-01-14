ab3c6e4e90a8f185980530267aad0468fb6684b6
Jada Mathele <jadamaadimo.mathele@gmail.com>
2025-01-14 15:06:33 +0200

    first commit

---
+++ RestaurantList/Controllers/RestaurantList.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantList.Data;
+using RestaurantList.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace RestaurantList.Controllers
+{
+    public class RestaurantList : Controller
+    {
+        private readonly RestaurantListContext _context;
+
+        //variable constructor
+        //dependancy injection initialises _context
+        public RestaurantList(RestaurantListContext context)
+        {
+            _context = context;
+        }
+
+        //asynchronously fetches Restaurant records from database table
+        public async Task<IActionResult> Index() 
+        {
+            //renders Index.cshtml
+            return View(await _context.Restaurants.ToListAsync());
+        }
+
+    }
+}
