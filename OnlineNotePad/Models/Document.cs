6e864e6e0387089af5e06fb9f14d54f68b215cc1
Jada Mathele <jadamaadimo.mathele@gmail.com>
2025-01-20 10:47:21 +0200

    first commit

---
+++ OnlineNotePad/Models/Document.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineNotePad.Models
+{
+    public class Document
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        //user must be authenticated to create a document
+        [Required]
+        public string? UserId { get; set; }
+        //links user to documents they create
+        [ForeignKey("UserId")]
+        public IdentityUser User { get; set; }
+    }
+}
